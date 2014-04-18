eee404cc7cefaf87319d3d5b1b0e8180c83eaea3
Daniel Filho <daniel@proximadica.com>
2014-04-18 04:33:58 -0300

    test remote origin

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
 		<footer>
-			
 		</footer>
 		<script src="<?php bloginfo('template_url') ?>/js/libs/jquery-1.11.0.min.js"></script>
 		<script src="<?php bloginfo('template_url') ?>/all.js"></script>
